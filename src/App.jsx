62877b1dfed1bc67b8a4ef5dc6a1d9f7253c46fe
xyieko <emmanuel.ontweka@student.moringaschool.com>
2025-04-15 19:30:53 +0300

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,38 @@ import { TODOS } from './data/data';
 function App() {
 
   const [todos, setTodos] = useState(TODOS);
+  const [todoInput, setTodoInput] = useState("")
 
   console.log(todos);
-  
-  
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e);
-    
-  }
+ 
+    const newTodo = 
+    {
+      id:(todos.length + 1),
+      body:todoInput
+    }
 
+setTodos([...todos, newTodo]);
+setTodoInput("");
+  }
 
   return (
    <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="todo" />
+        <input type="text" name="todo"value={todoInput} onChange={e =>{ setTodoInput(e.target.value);
+        }}required/>
         <button type='submit'>Add Todo</button>
       </form>
 
-      <h2>My todos</h2>
-      <Todos todos={todos} setTodos={setTodos} />
-      <Button />
-      <Button greeting="jambo" />
-      <Button greeting="konichiwa" />
-
+  <h2>My todos</h2>
+  <Todos todos={todos} setTodos={setTodos} />
+  <Button />
+  <Button greeting="jambo" />
+  <Button greeting="konichiwa" />
    </div>
   )
 }
